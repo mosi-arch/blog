43b134fb2bcb210cfd7c5b8407cd6b8fe0048ef2
mosi <99150661+mosi-sol@users.noreply.github.com>
2023-02-13 00:49:49 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Home({ posts }) {
             </a>
           </Link>
           <p className='text-cyan-800 p-4'>{frontmatter.metaDesc}</p>
-          <p className='text-sky-600 p-4'>{frontmatter.tags}</p>
+          <p className='text-sky-600 p-4'>{frontmatter.article}</p>
         </div> 
       ))}
     </div>
