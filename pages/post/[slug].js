e7937039231343f0534041aa0d4d50f122218508
mosi <99150661+mosi-sol@users.noreply.github.com>
2023-02-12 22:04:05 +0330

    Update [slug].js

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -32,7 +32,8 @@ export default function PostPage({ frontmatter, content }) {
       <h1 className='text-2xl'>{frontmatter.title}</h1>
       <div className='grid grid-cols-1' dangerouslySetInnerHTML={{ __html: md().render(content) }} />
     </div>
-    <a href="https://github.com/mosi-sol"><img src="https://awesome-github-stats.azurewebsites.net/user-stats/mosi-sol?cardType=level&theme=github&preferLogin=false&Ring=8B4BDD&Title=8F51DD)](https://git.io/awesome-stats-card" /></a>
-    <a href="https://github.com/mosi-sol"><img src="https://github-readme-stats.vercel.app/api/top-langs/?username=mosi-sol&layout=compact" /></a>
+    <hr>
+    <img src="https://awesome-github-stats.azurewebsites.net/user-stats/mosi-sol?cardType=level&theme=github&preferLogin=false&Ring=8B4BDD&Title=8F51DD)](https://git.io/awesome-stats-card" />
+    <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=mosi-sol&layout=compact" />
   );
 }
