d358e695422273cf214e1d65fdc74bf4e5d3f58a
mosi <99150661+mosi-sol@users.noreply.github.com>
2023-02-13 00:06:51 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import matter from 'gray-matter';
 import Image from 'next/image';
 import Link from 'next/link';
+import Head from 'next/head';
 
 export async function getStaticProps() {
   const files = fs.readdirSync('posts');
@@ -25,6 +26,9 @@ export async function getStaticProps() {
 
 export default function Home({ posts }) {
   return (
+    <Head>
+      <title>Sol Blog</title>        
+    </Head>
     <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-4 md:p-0'>
       {posts.map(({ slug, frontmatter }) => (
         <div
