8ba10079580741eaffeccf8c07bc1ef19058ba34
mosi <99150661+mosi-sol@users.noreply.github.com>
2023-02-14 01:57:40 +0330

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -8,26 +8,26 @@ export default function Layout({ children }) {
             <div class="navbar-collapse collapse grow items-center" id="navbarSupportedContentX">
             <ul class="navbar-nav mr-auto flex flex-row">
               <li class="nav-item">
-                <h3 class="block pr-2 lg:px-2 py-2 text-black mr-5 text-xl" data-mdb-ripple="true" data-mdb-ripple-color="light">SOL-BLOG</h3>
+                <h3 class="block pr-2 lg:px-2 py-2 text-black mr-5 text-xl">SOL-BLOG</h3>
               </li>
               <li class="nav-item">
                 <Link href='/'>
-                <a class="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light">Home</a>
+                <a class="nav-link block pr-2 lg:px-2 py-2 text-gray-600">Home</a>
                 </Link>
               </li>              
               <li class="nav-item">
                 <Link href='https://github.com/mosi-sol'>
-                <a class="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light">Github</a>
+                <a class="nav-link block pr-2 lg:px-2 py-2 text-gray-600">Github</a>
                 </Link>
               </li>
               <li class="nav-item">
                 <Link href='https://sol-app.github.io/resume/'>
-                <a class="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light">Resume</a>
+                <a class="nav-link block pr-2 lg:px-2 py-2 text-gray-600">Resume</a>
                 </Link>
               </li>
               <li class="nav-item">
                 <Link href='https://sol-app.github.io/mosi-sol/'>
-                <a class="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light">Portfolio</a>
+                <a class="nav-link block pr-2 lg:px-2 py-2 text-gray-600">Portfolio</a>
                 </Link>
               </li>
             </ul>
