311418f2283ba15073b02968c0ac640296dd37f5
mosi <99150661+mosi-sol@users.noreply.github.com>
2023-02-13 04:41:23 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -28,21 +28,23 @@ export default function Home({ posts }) {
   return (
     <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-4 md:p-0'>
       {posts.map(({ slug, frontmatter }) => (
-        <div
-          key={slug}
-          className='border border-black border-2 border-double m-2 rounded-lg shadow-lg overflow-hidden flex flex-col'
-        >
-        <Head>
-          <title>Sol Blog</title>        
-        </Head>
-          <Link href={`/post/${slug}`}>
-            <a>
-              <h1 className='px-2 pt-2 text-sky-800 text-lg'>{frontmatter.title}</h1>
-            </a>
-          </Link>
-          <p className='text-black p-2'>{frontmatter.metaDesc}</p>
-          <p className='text-sky-600 p-2'>{frontmatter.article}</p>
-        </div> 
+          <div
+            key={slug}
+            className='p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4'
+          >
+              <Head>
+                <title>Sol Blog</title>        
+              </Head>
+              <div>
+                <Link href={`/post/${slug}`}>
+                  <a>
+                    <h1 className='text-xl font-medium text-black'>{frontmatter.title}</h1>
+                  </a>
+                </Link>
+                <p className='text-slate-500'>{frontmatter.metaDesc}</p>
+                <p className='text-sky-600'>{frontmatter.article}</p>
+              </div>  
+          </div> 
       ))}
     </div>
   );
