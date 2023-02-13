51e65e12d96f69f3895a77e3da09891a5325fbbb
mosi <99150661+mosi-sol@users.noreply.github.com>
2023-02-14 00:34:13 +0330

    Update [slug].js

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -33,10 +33,11 @@ export default function PostPage({ frontmatter, content }) {
         <Head>
           <title>Sol Blog | {frontmatter.title}</title>        
         </Head>
-        <h1 className='text-2xl'>
-          {frontmatter.title} 
+        <h1>
+          <span className='text-2xl'>{frontmatter.title}</span>
+          <small className='text-sm bg-blue-500 text-white rounded-md p-2 shadow hover:shadow-lg'>{frontmatter.article}</small>
         </h1>
-        <small className='text-sm bg-blue-500 text-white rounded-md p-2 shadow hover:shadow-lg'>{frontmatter.article}</small>
+        
         <div className='shadow-lg grid grid-cols-1' dangerouslySetInnerHTML={{ __html: md().render(content) }} />
       </div>
     
