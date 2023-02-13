35ed39a0ec5c9c3f0f36ec65b22536770c9676fd
mosi <99150661+mosi-sol@users.noreply.github.com>
2023-02-14 01:14:04 +0330

    Update [slug].js

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -36,9 +36,15 @@ export default function PostPage({ frontmatter, content }) {
         <h1>
           <span className='text-2xl mr-4'>{frontmatter.title}</span>
           <small className='text-sm bg-blue-500 text-white rounded-md p-2 font-thin uppercase'>{frontmatter.article}</small>
-        </h1>
-        
+        </h1>        
         <div className='shadow-lg grid grid-cols-1' dangerouslySetInnerHTML={{ __html: md().render(content) }} />
+        <nav class="rounded-md w-full">
+          <ol class="list-reset flex">
+            <li><a href="/" class="text-blue-600 hover:text-blue-700">Home</a></li>
+            <li><span class="text-gray-500 mx-2">/</span></li>
+            <li class="text-gray-500">{frontmatter.title}</li>
+          </ol>
+        </nav>
       </div>
     
   );
